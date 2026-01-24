421b26c15740fc1e7e79ebaa0cc9b5a113db1a46
kesavan B <81011748+kesavan99@users.noreply.github.com>
2026-01-24 09:34:04 +0000

    remove

--- routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/contact/contactController');
-
-router.post('/send', contactController.sendContactMessage);
-
-module.exports = router;
